cbc297896b5d1f1b70371d888f55a53b5183d1b4
jessiiishin <jessiiishin@gmail.com>
2026-01-17 16:13:48 +0900

    feat: home page basic

---
+++ src/pages/Home.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+
+function Home() {
+  return (
+    <div>
+      <header>
+        <h1>Jessi Shin</h1>
+      </header>
+      
+      <main>
+        <section>
+          <p>
+            # Computer Science @ Brown University <br/>
+            # Front-end / Full Stack Developer <br/>
+            # Game Developer <br/>
+          </p>
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default Home;
