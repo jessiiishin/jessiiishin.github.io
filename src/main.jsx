559bdf2f99b7824ddb20ddcd7db57167050917a3
jessiiishin <jessiiishin@gmail.com>
2026-01-18 20:59:39 +0900

    feat: basic projects and list

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,6 @@ createRoot(document.getElementById('root')).render(
   <HashRouter>
     <StrictMode>
       <App />
-    </StrictMode>,
+    </StrictMode>
   </HashRouter>
 )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,30 @@
 import { useState } from 'react'
+import './home.css'
 
 function Home() {
   return (
-    <div>
-      <header>
-        <h1>Jessi Shin</h1>
+    <div className="home-container">
+      <header className="header">
+        <h1>JESSI SHIN</h1>
       </header>
       
       <main>
+        <section className="projects-section">
+          <h2 className="subtitle">Projects</h2>
+          <div className="projects-list">
+            <a href="">ChewCheck</a>
+            <a href="https://devpost.com/software/carbonchain-m2hxz4">CarbonChain</a>
+            <a href="https://brownrisdgames.itch.io/mosquito-simulator">Mosquito Simulator</a>
+            <a href="https://github.com/jessiiishin/workflow">WorkFlow</a>
+            <a href="https://project-brown-mock-trial.vercel.app/join-us">Brown Mock Trials (Join Us)</a>
+          </div>
+        </section>
         <section>
-          <p>
-            # Computer Science @ Brown University <br/>
-            # Front-end / Full Stack Developer <br/>
-            # Game Developer <br/>
-          </p>
+          <h2 className="subtitle">Links</h2>
+          <div className="projects-list">
+            <a href="https://docs.google.com/document/d/1KBAzdjLaOonfQr8kIbvjmU9exbH0lm-R-dvGjQA1cuE/edit?usp=sharing">Resume</a>
+            <a href="http://github.com/jessiiishin">Github</a>
+          </div>
         </section>
       </main>
     </div>
